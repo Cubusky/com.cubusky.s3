b0c9a6cbb63d8dffa74902a6119491489c749f22
CaseyHofland <CaseyHofland@users.noreply.github.com>
2024-03-21 22:24:12 +0000

    deploy: fc15a87e37ea64270ce78ca72c90a41025eac502

--- Runtime/ICredentials.cs
+++
@@ -1,9 +0,0 @@
-using Amazon.Runtime;
-
-namespace Cubusky.S3
-{
-    public interface ICredentials
-    {
-        AWSCredentials credentials { get; }
-    }
-}
--- Runtime/IRegionEndpoint.cs
+++
@@ -1,9 +0,0 @@
-using Amazon;
-
-namespace Cubusky.S3
-{
-    public interface IRegionEndpoint
-    {
-        RegionEndpoint region { get; }
-    }
-}
